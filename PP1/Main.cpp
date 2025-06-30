e54084944c00ebef5c84021e55a3aa9853809d1f
R-Bleach <loljk366@gmail.com>
2025-06-30 11:34:42 +0200

    3D start

--- PP1/Main.cpp
+++ PP1/Main.cpp
@@ -2,27 +2,38 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
 
-	//Vertex coordinates of the triangle
-	GLfloat vertices[] =
-	{ //     COORDINATES     /        COLORS      
-		-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 0.0f, 0.0f,	 // Lower left corner
-		-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f,// Upper left corner
-		 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f, 1.0f,// Upper right corner
-		 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f, 1.0f, 0.0f // Lower right corner
-	};
+//Vertex coordinates of the triangle
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS			/	TexCoord
+	-0.5f,  0.0f, 0.5f,     0.83f, 0.70f, 0.44f,		0.0f, 0.0f,
+	-0.5f,  0.0f,-0.5f,     0.83f, 0.70f, 0.44f,  		5.0f, 0.0f,
+	 0.5f,  0.0f,-0.5f,     0.83f, 0.70f, 0.44f,		0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,		5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,		2.5f, 2.5f
+};
 
-	// Indices for vertices order
-	GLuint indices[] =
-	{
-		0, 2, 1, // Upper triangle
-		0, 3, 2 // Lower triangle
-	};
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
+};
+
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main() {
 	//Initiate glfw
@@ -38,7 +49,7 @@ int main() {
 
 
 	//Create a window object that is 800 by 600 and is named PersonalPortfolio1 and is not fullscreen
-	GLFWwindow* window = glfwCreateWindow(800, 600, "PersonalPortfolio1", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "PersonalPortfolio1", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create a window" << std::endl;
@@ -53,21 +64,21 @@ int main() {
 
 	//Specify the viewport of OpenGl. 
 	//Here the start position is 0,0 and end position is 800,600
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 
-	
+
 
 	Shader shaderProgram("default.vert", "default.frag");
 
 	VAO VAO1;
 	VAO1.Bind();
 
-	VBO VBO1(vertices,sizeof(vertices));
+	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3 , GL_FLOAT, 8*sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3*sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -79,7 +90,7 @@ int main() {
 
 	int widthImg, heightImg, numColCh;
 	stbi_set_flip_vertically_on_load(true);
-	unsigned char* bytes = stbi_load("mega-gengar.png", &widthImg, &heightImg, & numColCh,0);
+	unsigned char* bytes = stbi_load("brick.png", &widthImg, &heightImg, &numColCh, 0);
 
 	GLuint texture;
 	glGenTextures(1, &texture);
@@ -100,6 +111,12 @@ int main() {
 
 	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
+
 	shaderProgram.Activate();
 
 	glUniform1i(tex0Uni, 0);
@@ -108,15 +125,37 @@ int main() {
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0, 0.5f, 0.5f, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		//Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 60) {
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 		glUniform1f(uniID, 0.5f);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		//Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		//Draw the triangle using GL_TRIANGLES primitive
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		//Take care of all GLFW events
@@ -124,13 +163,13 @@ int main() {
 	}
 
 	//Delete all the objects
-	
+
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
 	shaderProgram.Delete();
 
-	glDeleteTextures(1,&texture);
+	glDeleteTextures(1, &texture);
 
 	//Delete window
 	glfwDestroyWindow(window);
